5c3363187038b2d0d74b41895e422d7adb67b186
Charlie Burnham <felixtheslayer@gmail.com>
2023-06-17 12:53:19 -0500

    Fixing indirect light

--- src/the_voxel_gi_doesnt_want_to_start_working.gd
+++ src/the_voxel_gi_doesnt_want_to_start_working.gd
@@ -1,10 +1,5 @@
 extends VoxelGI
 
 func _ready():
-	data = null
-	$Timer.start()
-
-
-func _on_timer_timeout():
-	print("ooh!")
-	data = load("res://baked/test_world_interior_voxel_gi_data.res")
+	await get_tree().create_timer(0.3).timeout
+	set_base(data.get_rid())
